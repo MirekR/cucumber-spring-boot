57c0f38faea6baa4022b0e2d186b68025f547aeb
mirekrousal@centrum.cz <mirekrousal@centrum.cz>
2018-05-25 19:44:24 +0100

    Removing web starter

--- src/test/java/cz/mirek/cucumber/config/RestTemplateConfig.java
+++ src/test/java/cz/mirek/cucumber/config/RestTemplateConfig.java
@@ -3,7 +3,6 @@ package cz.mirek.cucumber.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.DefaultResponseErrorHandler;
 import org.springframework.web.client.RestTemplate;
 
@@ -11,8 +10,7 @@ import org.springframework.web.client.RestTemplate;
 public class RestTemplateConfig {
     @Bean
     RestTemplate restTemplate() {
-        HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
-        RestTemplate template = new RestTemplate(requestFactory);
+        RestTemplate template = new RestTemplate();
         // Rest template should not throw exceptions on server errors, but return the entity to us
         template.setErrorHandler(new DefaultResponseErrorHandler() {
             @Override
